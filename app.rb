99997cdab95ff8091e2017c3bd22b21045d4d0c7
Elana Koren <ekoren@pivotal.io>
2016-11-21 10:48:18 -0800

    Allow setting the header and message with environment variables [#134711445]
    Signed-off-by: Ian Fisher <loktar@gmail.com>

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/**' do
-  @message = ENV['MAINTENANCE_MESSAGE'] || 'Under construction'
+  @header = ENV['MAINTENANCE_HEADER'] || 'This app is undergoing maintenance.'
+  @message = ENV['MAINTENANCE_MESSAGE'] || 'If you need help, join the <a href="https://pivotal.slack.com/messages/iad_app_status/">#iad_app_status</a> Slack channel or send an ask ticket.'
   erb :index
 end
